3b6fa4db9311663d777cdf7af33e84a5bd26a5e1
Murkoth <jeevan.murkoth@intel.com>
2016-08-07 23:51:22 -0500

    Rest & Spread

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@
  import * as constantDemo from './constant.demo';
  import *  as arrowDemo from './arrowfunc.demo';
  import * as desctru from './destructuring.demo';
+ import * as restdemo from './restSpread.demo';
 let m=20;
 console.log(m);
 //constant Demo
@@ -9,3 +10,5 @@ console.log(`********************`);
 constantDemo.demoCall();
 console.log(arrowDemo.sqrt(16));
 desctru.destruct();
+console.log(restdemo.restdemo(1,2,3,4,5,6,7,8));
+console.log(restdemo.defValue(4));
---
+++ app/restSpread.demo.js
@@ -0,0 +1,13 @@
+function f (x, y, ...a) {
+    console.log(`x:${x} y:${y} a:${a}`);
+    return (x + y) * a.length
+}
+function defValue(x, y=12) {
+  // y is 12 if not passed (or passed as undefined)
+  return x + y;
+}
+
+module.exports={
+    restdemo:f,
+    defValue:defValue
+};
