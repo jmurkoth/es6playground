bab4b46c2e8d5dec3e70fa2c8686089f0e3dcc5f
Murkoth <jeevan.murkoth@intel.com>
2016-08-07 23:10:57 -0500

    destructuring

---
+++ app/destructuring.demo.js
@@ -0,0 +1,30 @@
+
+export function destruct(){
+    //array destructuring
+    var [a,, , b] = [1,2,3,4,5,6];
+    console.log(`the value of a is ${a} and b is ${b}`);
+    //object destructuring
+    const obj = { first: 'Jane', last: 'Doe' };
+    const {first: f, last: l} = obj;
+    console.log(`the value of f is ${f} and l is ${l}`);
+    const {first,last}=obj;
+    console.log(`the value of first is ${first} and last is ${last}`);
+    //default values
+    var a, b;
+    [a=5, b=7] = [1];
+    console.log(a); // 1
+    console.log(b); // 7
+    // default parameters
+    drawES6Chart({cords: { x: 18, y: 30 }, radius: 30});
+    someMethod({ name: "bar", val: 42 });
+}
+function drawES6Chart({size = 'big', cords = { x: 0, y: 0 }, radius = 25} = {}) {
+  console.log(size, cords, radius);
+  // do some chart drawing
+}
+function someMethod ({ name, val }) {
+    console.log(name, val)
+}
+module.exports  ={
+        destruct:destruct
+};
--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
  import {} from './class.demo';
  import * as constantDemo from './constant.demo';
  import *  as arrowDemo from './arrowfunc.demo';
-
+ import * as desctru from './destructuring.demo';
 let m=20;
 console.log(m);
 //constant Demo
 console.log(`********************`);
 constantDemo.demoCall();
 console.log(arrowDemo.sqrt(16));
+desctru.destruct();
